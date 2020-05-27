c2cbfdb660e67361d17b5f5f5762ff941e2cc21c
joseph reymond <josephreymond2011@gmail.com>
2020-05-27 18:23:41 +0530

    sdf

--- memcache1.py
+++ memcache1.py
@@ -1,11 +1,18 @@
 from twisted.internet import reactor, protocol
 from twisted.protocols.memcache import MemCacheProtocol, DEFAULT_PORT
+
 d = protocol.ClientCreator(reactor, MemCacheProtocol
         ).connectTCP("localhost", DEFAULT_PORT)
+
+print(d)
+print('connected')
 def doSomething(proto):
     # Here you call the memcache operations
     print('do something')
     return proto.set("mykey", "a lot of data")
 
+
+print('before callback')
 d.addCallback(doSomething)
+print('after callback')
 reactor.run()
---
+++ txredis1.py
@@ -0,0 +1,25 @@
+import txredisapi as redis
+
+from twisted.internet import defer
+from twisted.internet import reactor
+
+
+@defer.inlineCallbacks
+def main():
+    print('insid main')
+    rc = yield redis.Connection()
+    print (rc)
+
+    yield rc.set("foo", "bar")
+    v = yield rc.get("foo")
+    print ("foo:", repr(v))
+
+    yield rc.disconnect()
+    print('disconnect'
+    )
+
+
+if __name__ == "__main__":
+    main().addCallback(lambda ign: reactor.stop())
+    print('aftercallback')
+    reactor.run()
